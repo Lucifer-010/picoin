808e495bedcb769810d5c822c1b0fd514ab0aad5
Android_Lucifer <agwumafamelody@yahoo.com>
2024-08-22 19:02:04 +0100

    Update settings.py

--- picoin/settings.py
+++ picoin/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k(f%n)hn^2yvvsz^p5y2u18i*5=y45#pia4l=o_&%w6^4ygq0g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["pichainmainnet.com","picoin.onrender.com","picoin-t41c.onrender.com","www.pinetblockchain.com",'localhost','127.0.0.1']
 RENDER_EXTERNAL_HOSTNAME = 'web-production-7269c.up.railway.app'
